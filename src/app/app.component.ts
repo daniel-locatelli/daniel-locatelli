8891c14dcf0fcdd95871802e8b5825715f1fb891
daniel-locatelli <daniel.nlocatelli@gmail.com>
2023-12-08 20:40:34 +0100

    Created a Hello firebase test. Copied the comming soon html to this repos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { Firestore, addDoc, collection } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,15 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'daniel-locatelli';
+export class AppComponent implements OnInit {
+  title: string = 'daniel-locatelli';
+
+  constructor(private firestore: Firestore){
+
+  }
+
+  public ngOnInit(): void {
+    const testCollection = collection(this.firestore, 'test');
+    addDoc(testCollection, {text: "Hello firebase"});
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,11 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"daniel-locatelli","appId":"1:878497542260:web:94012e698aaa065759a121","storageBucket":"daniel-locatelli.appspot.com","locationId":"europe-west","apiKey":"AIzaSyBFAK6BMmJFewLSmgRTbbJc44yfFpwjiRI","authDomain":"daniel-locatelli.firebaseapp.com","messagingSenderId":"878497542260","measurementId":"G-4199F8L5CV"}))), importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideFirestore(() => getFirestore()))]
 };
